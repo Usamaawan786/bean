3d17ed91e84e9b78a3728ac69dc54cdb8bbae42f
Usama Ameer <usamaameer308@gmail.com>
2026-01-28 03:16:24 +0500

    fixes

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.beancoffee.app',
+  appName: 'BEAN Coffee',
+  webDir: 'dist'
+};
+
+export default config;
